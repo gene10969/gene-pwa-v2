44eb67fed7fc1af59445a5c441779d76a2dbc610
gene10969 <onokrk@gmail.com>
2026-05-12 05:19:41 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="gene-pwa-cache-v2";
+const CACHE_NAME="gene-pwa-cache-v3";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./assets/style.css","./assets/app.js","./assets/icon-192.svg","./assets/icon-512.svg"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim()});
